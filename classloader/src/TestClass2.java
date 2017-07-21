06b08707c4777fef1cf8e1c603017a349b5a8bff
4kix <falko.ilya@gmail.com>
2017-07-21 15:01:16 +0300

    Add ObjectSizer class

---
+++ classloader/src/TestClass2.java
@@ -0,0 +1,19 @@
+
+public class TestClass2 implements TestClassInterface {
+	public double field1;
+	public int field2;
+	public long field3;
+	public short field4;
+	public float field5;
+	public float field6;
+	public double[] array = new double[30];
+	
+	static {
+			System.out.println("TestClass2 init");
+	}
+	
+	@Override
+	public void test() {
+		System.out.println("test method of TestClass2 successfully called");
+	}
+}
